8197da6b03ab06c3e3706e79181606f6d1f760cb
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-30 19:04:33 -0300

    Apolls Studio

--- 02-todo/src/app.module.ts
+++ 02-todo/src/app.module.ts
@@ -3,13 +3,17 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      // playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault()
+      ],
+      playground: false,
     }),
     HelloWorldModule
   ],
