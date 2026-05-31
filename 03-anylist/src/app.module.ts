98543a6a2fad793a63f2c952d5163c3982f0c585
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-31 13:14:47 -0300

    Conectar base de datos a Nest

--- 03-anylist/src/app.module.ts
+++ 03-anylist/src/app.module.ts
@@ -4,9 +4,12 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
@@ -14,6 +17,16 @@ import { ItemsModule } from './items/items.module';
       plugins: [
         ApolloServerPluginLandingPageLocalDefault()
       ]
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
     }),
     ItemsModule,
   ],
