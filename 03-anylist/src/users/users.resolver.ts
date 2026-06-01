27a8c43f662404fcd3a45b2ce002f515ecac373c
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-01 13:13:38 -0300

    Proteger todos los queries de los usuarios

--- 03-anylist/src/users/users.resolver.ts
+++ 03-anylist/src/users/users.resolver.ts
@@ -1,15 +1,21 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/args/roles.arg';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { ValidRoles } from '../auth/enums/valid-roles.enums';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
   findAll(
-    @Args() validRoles: ValidRolesArgs
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.admin]) user: User,
   ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles);
   }
