0acfd9518564e026956bd37c1dfe9a47c9333255
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-01 19:28:50 -0300

    Actualizar un usuario

--- 03-anylist/src/users/dto/inputs/create-user.input.ts
+++ 03-anylist/src/users/dto/inputs/create-user.input.ts
@@ -1,7 +1,20 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  @IsEmail()
+  email: string;
+
+  @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  fullName: string;
+
+  @Field(() => String)
+  @MinLength(6)
+  @IsString()
+  password: string;
 }
--- 03-anylist/src/users/users.service.ts
+++ 03-anylist/src/users/users.service.ts
@@ -63,8 +63,24 @@ export class UsersService {
     }
   }
 
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserInput: UpdateUserInput, updateBy: User): Promise<User> {
+
+    try {
+      const user = await this.usersRepository.preload({
+        ...updateUserInput,
+        id
+      });
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      user.lastUpdateBy = updateBy;
+
+      return await this.usersRepository.save(user);
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
   }
 
   async block(id: string, adminUser: User): Promise<User> {
