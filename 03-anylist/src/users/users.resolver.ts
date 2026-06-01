79c5265faf828e8082ac41c031134cba66dfdf0e
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-01 13:17:44 -0300

    Buscar usuario por id

--- 03-anylist/src/users/users.resolver.ts
+++ 03-anylist/src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { UseGuards } from '@nestjs/common';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
@@ -21,9 +21,11 @@ export class UsersResolver {
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
-    throw new Error('Not implemented')
-    // return this.usersService.find(id);
+  findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User,
+  ): Promise<User> {
+    return this.usersService.findOneById(id);
   }
 
   @Mutation(() => User)
