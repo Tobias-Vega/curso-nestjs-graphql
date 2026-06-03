e27017cbbbb16395a596fb2c895b7ffcd15003c4
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-03 13:31:42 -0300

    ResolveField con información del padre - Fin sección 7

--- 03-anylist/src/items/items.service.ts
+++ 03-anylist/src/items/items.service.ts
@@ -71,4 +71,14 @@ export class ItemsService {
 
     return { ...item, id };
   }
+
+  async itemCountByUser(user: User): Promise<number> {
+    return this.itemRepository.count({
+      where: {
+        user: {
+          id: user.id
+        }
+      }
+    })
+  }
 }
--- 03-anylist/src/users/users.module.ts
+++ 03-anylist/src/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ItemsModule } from '../items/items.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ItemsModule,
+  ],
   providers: [UsersResolver, UsersService],
   exports: [
     // TypeOrmModule,
