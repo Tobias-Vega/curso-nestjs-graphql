c840f59242744bf33e363c67fb0bcb1971da6bdd
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-04 14:02:34 -0300

    Aplicar filtros a sub-campos

--- 03-anylist/src/users/users.resolver.ts
+++ 03-anylist/src/users/users.resolver.ts
@@ -2,12 +2,14 @@ import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, ID, ResolveField, Int, Parent } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { Item } from '../items/entities/item.entity';
 import { ValidRolesArgs } from './dto/args/roles.arg';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { ValidRoles } from '../auth/enums/valid-roles.enums';
 import { UpdateUserInput } from './dto/inputs/update-user.input';
 import { ItemsService } from '../items/items.service';
+import { PaginationArgs, SearchArgs } from '../common/dto/args';
 
 @Resolver(() => User)
 @UseGuards(JwtAuthGuard)
@@ -55,5 +57,15 @@ export class UsersResolver {
     @Parent() user: User
   ): Promise<number> {
     return await this.itemsService.itemCountByUser(user);
+  }
+
+  @ResolveField(() => [Item], { name: 'items', nullable: true })
+  async getItemsByUser(
+    @CurrentUser([ValidRoles.admin]) adminUser: User,
+    @Parent() user: User,
+    @Args() paginationsArgs: PaginationArgs,
+    @Args() searchArgs: SearchArgs,
+  ): Promise<Item[]> {
+    return await this.itemsService.findAll(user, paginationsArgs, searchArgs)
   }
 }
