e764ea20243a1c5b90654f1c341875adcc773b54
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-05 12:07:15 -0300

    Actualizar un ListItem

--- 03-anylist/src/list-item/list-item.resolver.ts
+++ 03-anylist/src/list-item/list-item.resolver.ts
@@ -25,10 +25,10 @@ export class ListItemResolver {
     return await this.listItemService.findOne(id);
   }
 
-  // @Mutation(() => ListItem)
-  // updateListItem(@Args('updateListItemInput') updateListItemInput: UpdateListItemInput) {
-  //   return this.listItemService.update(updateListItemInput.id, updateListItemInput);
-  // }
+  @Mutation(() => ListItem)
+  async updateListItem(@Args('updateListItemInput') updateListItemInput: UpdateListItemInput): Promise<ListItem> {
+    return await this.listItemService.update(updateListItemInput.id, updateListItemInput);
+  }
 
   // @Mutation(() => ListItem)
   // removeListItem(@Args('id', { type: () => Int }) id: number) {
--- 03-anylist/src/list-item/list-item.service.ts
+++ 03-anylist/src/list-item/list-item.service.ts
@@ -55,8 +55,20 @@ export class ListItemService {
   }
   
 
-  update(id: number, updateListItemInput: UpdateListItemInput) {
-    return `This action updates a #${id} listItem`;
+  async update(id: string, updateListItemInput: UpdateListItemInput): Promise<ListItem> {
+
+    const { listId, itemId, ...rest } = updateListItemInput
+
+    const listItem = await this.listItemsRepository.preload({
+      ...rest,
+      list: { id: listId },
+      item: { id: itemId },
+    });
+
+    if (!listItem) throw new NotFoundException('ListItem not found');
+
+    return this.listItemsRepository.save(listItem);
+
   }
 
   remove(id: number) {
